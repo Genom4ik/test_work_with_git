35d64f54334cb417e6a9a0867d6fb81dd08e06b8
Genom4ik <g.develop.85@gmail.com>
2026-04-24 17:06:59 +0300

    add var "city"

--- hello_git.py
+++ hello_git.py
@@ -1,8 +1,8 @@
-from name import name
+from name import name, city
 
 print(f"Hello Github: {name}.")
 
 def simple_func() -> str:
-    return "Hello world!"
+    return f"Hello world! I'm from {city}."
 
 print(simple_func())
